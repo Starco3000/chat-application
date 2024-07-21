e40f04e384a798bd260352924576340657f12791
Staco3000 <minhnguyen9686q6@gmail.com>
2024-07-21 23:53:18 +0700

    server chatapp with register and check login/password

---
+++ server/controller/checkEmail.js
@@ -0,0 +1,28 @@
+const UserModel = require('../models/UserModel');
+
+async function checkEmail(request, response) {
+  try {
+    const { email } = request.body;
+
+    const checkEmail = await UserModel.findOne({ email }).select('-password');
+
+    if (!checkEmail) {
+      return response.status(400).json({
+        message: 'User not exist',
+        error: true,
+      });
+    }
+    return response.status(200).json({
+      message: 'User verify',
+      data: checkEmail,
+      success: true,
+    });
+  } catch (error) {
+    return response.status(500).json({
+      message: error.message || error,
+      error: true,
+    });
+  }
+}
+
+module.exports = checkEmail;
