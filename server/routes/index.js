6347dfc8e5572a7ad7a3dc3228809f4981bd66cd
Staco3000 <minhnguyen9686q6@gmail.com>
2024-07-23 15:18:49 +0700

    make getUserDetails/UpdateUserDetails/Logout logic

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const registerUser = require('../controller/registerUser');
 const checkEmail = require('../controller/checkEmail');
 const checkPassword = require('../controller/checkPassword');
+const userDetails = require('../controller/userDetails');
+const logout = require('../controller/logout');
+const updateUserDetails = require('../controller/updateUserDetails');
 
 const router = express.Router();
 
@@ -11,5 +14,11 @@ router.post('/register', registerUser);
 router.post('/email', checkEmail);
 //check user password
 router.post('/password', checkPassword);
+//login user details
+router.get('/user-details', userDetails);
+//logout user
+router.get('/logout', logout);
+//update user details
+router.post('/update-user', updateUserDetails);
 
 module.exports = router;
