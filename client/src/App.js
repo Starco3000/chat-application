61a2ef8ce95c55a4b910bbe6bbe9ed06b9e2e8e1
Staco3000 <minhnguyen9686q6@gmail.com>
2024-07-26 15:43:49 +0700

    Make register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return <h1 className='text-3xl text-red-600'>Hello world</h1>;
+  return (
+    <main >
+      <Outlet />
+    </main>
+  );
 }
 
 export default App;
